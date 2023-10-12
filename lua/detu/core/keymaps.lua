3aa5e6e909cc7eaedd8d3d3844ba18efd2e53c06
Samuele De Tuglie <samuele.de.tuglie@gmail.com>
2023-10-12 16:32:15 +0200

    Add ufo

--- lua/detu/core/keymaps.lua
+++ lua/detu/core/keymaps.lua
@@ -1,4 +1,3 @@
--- set leader key to space
 vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
---
+++ lua/detu/plugins/ufo.lua
@@ -0,0 +1,21 @@
+return {
+	"kevinhwang91/nvim-ufo",
+	dependencies = { "kevinhwang91/promise-async" },
+	opts = {
+		filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy", "mason" },
+	},
+	config = function(_, opts)
+		vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+		vim.o.foldcolumn = "1"
+		vim.api.nvim_create_autocmd("FileType", {
+			group = vim.api.nvim_create_augroup("local_detach_ufo", { clear = true }),
+			pattern = opts.filetype_exclude,
+			callback = function()
+				require("ufo").detach()
+			end,
+		})
+
+		vim.opt.foldlevelstart = 99
+		require("ufo").setup(opts)
+	end,
+}
